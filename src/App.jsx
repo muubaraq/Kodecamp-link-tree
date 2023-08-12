77789b731791e320939efd498b9d33d97598a51a
mubaraq <sixthlogic@gmail.com>
2023-08-12 20:06:11 +0100

    linktree page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,60 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import dots from './assets/dots.svg'
+import kodeCampLogo from './assets/logo.svg'
+import kodeHauzIcon from './assets/KodeHauz.svg'
+import profileImg from './assets/profile_img.png'
+import gitHub from './assets/github.svg'
+import share from './assets/share.svg'
+import slack from './assets/slack.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <section class="parent-wrapper">
+        <div class="dots-div">
+                <img src={dots} class="dot-icon">
+                <img src={share} alt="share-icon" class="share-icon">
+            </div>
+        <div class="container">
+            <div class="imgdiv">
+                <img src={profileImg}>
+                <h1>John Doe</h1>
+            </div>
+            <div class="links-wrapper">
+                <div class="link-content">
+                    <a href="#" target="_blank">Twitter Link</a>
+                </div>
+                <div class="link-content">
+                    <a href="#" target="_blank">Kodecamp Team</a>
+                </div>
+                <div class="link-content">
+                    <a href="#" target="_blank">Kodecamp Books</a>
+                </div>
+                <div class="link-content">
+                    <a href="#" target="_blank">Python Books</a>
+                </div>
+                <div class="link-content">
+                    <a href="#" target="_blank">Background Check For Coders</a>
+                </div>
+            </div>
+            <div class="social-icons">
+                <img src={slack} class="img-slack">
+                <img src={gitHub}>
+            </div>
+            <hr>
+        </div>
+        <div class="kodecamp-assets">
+            <div class="kodecamp-assets-img">
+                <img src={logo} alt="kodecamp logo">
+            </div>
+            <p>KodeCamp Internship Task</p>
+            <div class="kodecamp-assets-img kodecamp-assets-img2">
+                <img src={kodeHauzIcon} alt="kodecamp logo">
+            </div>
+        </div>
+    </section>
     </>
   )
 }
